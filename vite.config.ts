08b488972f352050b5be88d7e4ec9d849d50bfa1
adbenitez <asieldbenitez@gmail.com>
2024-12-09 15:21:50 +0100

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { buildXDC, eruda, mockWebxdc } from "webxdc-vite-plugins";
+import { webxdcViteConfigNoLegacy } from "@webxdc/vite-plugins";
 import { defineConfig } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [buildXDC(), eruda(), mockWebxdc()],
+  ...webxdcViteConfigNoLegacy(),
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
